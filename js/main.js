4f76447ab06c36ebffac1e1f95d1fdfeea5039f7
P4anix <pancok_1@yahoo.it>
2023-02-20 16:19:41 +0100

    fix paswword

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const lastName = prompt('Perfavore inserisci il tuo cognome')
 const favoriteColor = prompt('Perfavore inserisci il tuo colore preferito')
 
 /* inseriamo il debug per la console */
-const generatedPassword = `Complimenti, ${firstName} ${lastName} ${favoriteColor}  89 sarà la tua password `
+const generatedPassword = `Complimenti, ${firstName}${lastName}${favoriteColor}89 sarà la tua password `
 console.log(generatedPassword);
 
 /* inseriamo il cambio di paragrafo per avere la password */
