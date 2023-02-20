17f8477a8acd2c868d9698af7cfb230d5f7bd9e8
P4anix <pancok_1@yahoo.it>
2023-02-20 15:56:09 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict'
+/* inseriamo il nome */
+const firstName = prompt('Perfavore inserisci il tuo nome')
+
+/* inseriamo il cognome */
+const lastName = prompt('Perfavore inserisci il tuo cognome')
+
+/* inseriamo il colore preferito */
+const favoriteColor = prompt('Perfavore inserisci il tuo colore preferito')
+
+/* inseriamo il debug per la console */
+const generatedPassword = `Complimenti, ${firstName} ${lastName} ${favoriteColor}  89 sarà la tua password `
+console.log(generatedPassword);
+
+/* inseriamo il cambio di paragrafo per avere la password */
+document.querySelector('p').innerHTML = generatedPassword
